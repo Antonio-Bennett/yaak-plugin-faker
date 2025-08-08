adc8d1b8adb662e9d39c2cf08c47bce87927762c
Gregory Schier <gschier1990@gmail.com>
2025-08-08 12:41:54 -0700

    Better README

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,46 @@ import {faker} from '@faker-js/faker';
 import {PluginDefinition} from '@yaakapp/api';
 
 const modules = [
-    "airline",
-    "animal",
-    "color",
-    "commerce",
-    "company",
-    "database",
-    "date",
-    "finance",
-    "git",
-    "hacker",
-    "image",
-    "internet",
-    "location",
-    "lorem",
-    "person",
-    "music",
-    "number",
-    "phone",
-    "science",
-    "string",
-    "system",
-    "vehicle",
-    "word",
+  "airline",
+  "animal",
+  "color",
+  "commerce",
+  "company",
+  "database",
+  "date",
+  "finance",
+  "git",
+  "hacker",
+  "image",
+  "internet",
+  "location",
+  "lorem",
+  "person",
+  "music",
+  "number",
+  "phone",
+  "science",
+  "string",
+  "system",
+  "vehicle",
+  "word",
 ];
 
 export const plugin: PluginDefinition = {
-    templateFunctions: modules.flatMap(name => {
-        // @ts-ignore
-        const module = faker[name];
-        return Object.keys(module).filter(n => n !== 'faker').map(functionName => ({
-            name: ['faker', name, functionName].join('.'),
-            args: [],
-            async onRender(_ctx, _args) {
-                const fakerFunction = module[functionName];
-                const result = fakerFunction();
-                if (typeof result !== 'string') {
-                    return JSON.stringify(result);
-                }
-                return result;
-            },
-        }));
-    }),
+  templateFunctions: modules.flatMap(name => {
+    // @ts-ignore
+    const module = faker[name];
+    return Object.keys(module).filter(n => n !== 'faker').map(functionName => ({
+      name: ['faker', name, functionName].join('.'),
+      args: [],
+      async onRender(_ctx, _args) {
+        const fakerFunction = module[functionName];
+        const result = fakerFunction();
+        if (typeof result !== 'string') {
+          return JSON.stringify(result);
+        }
+        return result;
+      },
+    }));
+  }),
 };
